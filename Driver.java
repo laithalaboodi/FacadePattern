be45ef768ae289aa1cdea9a3c2e544fb8c202ead
Laith <40428921+laithalaboodi@users.noreply.github.com>
2022-02-11 08:40:09 -0700

    first commit

---
+++ Driver.java
@@ -0,0 +1,23 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Driver {
+  
+  
+  public static void main(String[] args) { 
+    
+    BankAccountFacade accessingBank = new BankAccountFacade(12345678, 1234);
+    
+   
+    
+    accessingBank.withdrawCash(50.00);
+    
+    
+    accessingBank.withdrawCash(800.00);
+    
+    
+  }
+  
+  
+  
+}
